ea10a632e6d77fa040c5ddbb4e9cb5ab2252f07a
code-jpeg <112850823+code-jpeg@users.noreply.github.com>
2022-09-05 10:01:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ ctypes.windll.kernel32.SetConsoleTitleW("Aleks Group Finder")
 
 
 def groupfinder():
-    id = random.randint(1000000, 1150000)
+    id = random.randint(10000, 1229320)
     r = requests.get(f"https://www.roblox.com/groups/group.aspx?gid={id}") 
     if 'owned' not in r.text:
         re = requests.get(f"https://groups.roblox.com/v1/groups/{id}")
@@ -37,9 +37,9 @@ ____ _ _  _ ___  ____ ____
 """)
 
 #your webhook
-hook = input("[-] Enter your webhook url: "))
+hook = input("[-] Enter your webhook url: https://discord.com/api/webhooks/1016245363377721375/4P0yuBV9Eq1QwieoxKLdMvi7QYJbpXMV0B3q4sXWJN5N73OHPiBohp1yAqzdIYcNIOgP "))
 #number of threads
-threads = int(input("[-] How many threads: "))
+threads = int(input("[-] How many threads: 500 "))
 
 while True:
     if threading.active_count() <= threads:
