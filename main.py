ac157fe90064383df2c3890d46b13ec3f5150b92
code-jpeg <112850823+code-jpeg@users.noreply.github.com>
2022-09-05 10:03:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -37,9 +37,9 @@ ____ _ _  _ ___  ____ ____
 """)
 
 #your webhook
-hook = input("[-] Enter your webhook url: https://discord.com/api/webhooks/1016245363377721375/4P0yuBV9Eq1QwieoxKLdMvi7QYJbpXMV0B3q4sXWJN5N73OHPiBohp1yAqzdIYcNIOgP "))
+hook = input("[-] https://discord.com/api/webhooks/1016245363377721375/4P0yuBV9Eq1QwieoxKLdMvi7QYJbpXMV0B3q4sXWJN5N73OHPiBohp1yAqzdIYcNIOgP"))
 #number of threads
-threads = int(input("[-] How many threads: 500 "))
+threads = int(input("[-] 500 "))
 
 while True:
     if threading.active_count() <= threads:
